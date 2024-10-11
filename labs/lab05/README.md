ea01b2b3c91eefaee77dedf9b510b8ac4a2ddb48
Jordan Johnson <johnsj27@rpi.edu>
2024-10-10 23:09:46 -0400

    Lab5
    Lab5

---
+++ labs/lab05/README.md
@@ -0,0 +1,4 @@
+Working Link: http://johnsj27rpi.eastus.cloudapp.azure.com
+
+For this lab, I used ChatGPT to help me. I prompted it for instructions on how to make the comments reappear disappear on blur.
+I also used it to help me with the show name button.
--- labs/lab05/lab5.js
+++ labs/lab05/lab5.js
@@ -5,10 +5,95 @@ function validate(formObj) {
    // put your validation code here
    // it will be a series of if statements
 
+   var alertText = ""
+   var focused = false
+   
    if (formObj.firstName.value == "") {
-      alert("You must enter a first name");
-      formObj.firstName.focus();
-      return false;
+      alertText += "You must enter a first name\n";
+      if(!focused){
+         focused = true;
+         formObj.firstName.focus();
+      }
    }
-   return true;
+
+   if (formObj.lastName.value == ""){
+      alertText += "You must enter a last name\n";
+      if(!focused){
+         focused = true;
+         formObj.lastName.focus();
+      }
+   }
+
+   if (formObj.title.value == ""){
+      alertText += "You must enter a title\n";
+      if(!focused) {
+         focused = true;
+         formObj.title.focus();
+      }
+   }
+   
+   if (formObj.org.value == ""){
+      alertText += "You must enter an organization\n";
+      if(!focused) {
+         focused = true;
+         formObj.org.focus();
+      }
+   }
+
+   if (formObj.pseudonym.value == ""){
+      alertText += "You must enter a nickname\n";
+      if(!focused) {
+         focused = true;
+         formObj.pseudonym.focus();
+      }
+   }
+
+   if (formObj.comments.value == "" || formObj.comments.value == "Please enter your comments" ){
+      alertText += "You must enter comments\n";
+      if(!focused) {
+         focused = true;
+         formObj.comments.focus();
+      }
+   }
+
+   if (alertText == ""){
+      alert("Form submitted successfully!")
+      return true
+   } else {
+      alert(alertText);
+      return false
+   }
+   
+}
+function main() {
+
+}
+
+function resetComments() {
+   var comments = document.getElementById('comments');
+   if (comments.value === "") {
+      comments.value = "Please enter your comments";
+   }
+}
+
+
+function clearComments() {
+   var comments = document.getElementById('comments');
+   if (comments.value == "Please enter your comments") {
+      comments.value = "";
+   }
+}
+
+
+
+function displayName() {
+   var firstName = document.getElementById('firstName').value;
+   var lastName = document.getElementById('lastName').value;
+   var nickname = document.getElementById('pseudonym').value;
+   if (firstName && lastName && nickname) {
+      alert(firstName + " " + lastName + " is " + nickname);
+   } else {
+      alert("Fields Incomplete")
+   }
+   
 }
