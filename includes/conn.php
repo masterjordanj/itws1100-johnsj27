0a2e38cc8f059ad2d28393a608a4ade0d05e4033
Jordan Johnson <johnsj27@rpi.edu>
2024-12-05 18:46:52 -0500

    actual test 😞

---
+++ includes/conn.php
@@ -0,0 +1,11 @@
+<?php 
+
+$servername = "localhost";  // Server name (typically 'localhost' or IP address)
+$username = "phpmyadmin"; // Database username
+$password = "9p7OhMw0IFkY"; // Database password
+$dbname = "mySite";   // Database name
+
+// Create a connection using MySQLi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include('includes/codeheader.php'); ?>
+<link rel="stylesheet" href="style.css" type="text/css">
+<title>Home Page</title>
+</head>
+<body>
+   <div id="name">
+      <p>Jordan Johnson</p>
+   </div>
+
+   <?php include('includes/header.php')?>
+
+   <div id="row">
+      <div id="left"> 
+         <img src="labs/lab03/aboutphoto.jpeg" style="width:300px;height: 450px;">
+      </div>
+      <div id="right">
+         <p>Welcome!</p>
+      </div>
+   </div>
+</body>
+
+<?php include('includes/footer.php'); ?>
+
+</html>
